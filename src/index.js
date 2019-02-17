b6685d839a0b82f247fc5c1bc67c899cb0a7b5af
Justin Awrey <awreyjustin@gmail.com>
2019-02-16 22:39:18 -0800

    rotating cube

--- src/index.js
+++ src/index.js
@@ -1,30 +1,57 @@
-import * as THREE from 'three'
+import {
+    Scene,
+    PerspectiveCamera,
+    WebGLRenderer,
+    DirectionalLight,
+    BoxGeometry,
+    MeshPhongMaterial,
+    EdgesGeometry,
+    LineSegments,
+    LineBasicMaterial,
+    Mesh,
+    Group,
+} from 'three'
 import './index.css'
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
+const scene = new Scene()
+const camera = new PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 )
-
-const renderer = new THREE.WebGLRenderer()
+const canvas = document.createElement('canvas')
+const context = canvas.getContext('webgl2')
+const renderer = new WebGLRenderer({ canvas, context })
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setClearColor(0x000000, 1.0)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const directionalLight = new DirectionalLight(0xffffff, 0.7)
+directionalLight.position.set(8, 10, 8)
+scene.add(directionalLight)
 
-camera.position.z = 5
+const geometry = new BoxGeometry(1, 1, 1)
+const material = new MeshPhongMaterial({ color: 0x0000ff, shininess: 50 })
+const edges = new EdgesGeometry(geometry)
+const lines = new LineSegments(
+    edges,
+    new LineBasicMaterial({ color: 0x000000 })
+)
+const cube = new Mesh(geometry, material)
+const group = new Group()
+group.add(cube)
+group.add(lines)
+scene.add(group)
+
+camera.position.set(3, 2, 3)
+camera.lookAt(0, 0, 0)
 
 const animate = function() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    group.rotation.x += 0.01
+    group.rotation.y += 0.01
 
     renderer.render(scene, camera)
 }
